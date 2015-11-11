0d167a1c5cd770f140c1f72689b7225bb0c4cc58
yzuniversal <yzukonta@gmail.com>
2015-11-11 05:32:06 +0100

    Refactored the module into a factory function

--- preprocessor.js
+++ preprocessor.js
@@ -8,41 +8,47 @@
 const path = require('path');
 const babelJest = require('babel-jest');
 const transform = require('transform-jest-deps');
-const webpackConfig = require('./webpack.config.js');
-const aliases = Object.keys(webpackConfig.resolve.alias)
-.map(key => {
-  const value = path.join(webpackConfig.resolve.root, webpackConfig.resolve.alias[key]);
 
-  return {
-    key,
-    value,
-  };
-});
+function preprocessorFactory(options) {
+  const configLocation = options.configLocation || './webpack.config.js';
+  const webpackConfig = require(configLocation);
+  const aliases = Object.keys(webpackConfig.resolve.alias)
+  .map(key => {
+    const value = path.join(webpackConfig.resolve.root, webpackConfig.resolve.alias[key]);
 
-/**
- * A callback function for transform-jest-deps.
- * @param {string} require - Content of a require() statement. Automatically passed by transform-jest-deps.
- * @returns {string} Value passed in, either modifed by the function, or not.
- */
+    return {
+      key,
+      value,
+    };
+  });
 
-function resolve(require) {
-  for (let i = 0; i < aliases.length; i++) {
-    const alias = aliases[i];
-    const regex = new RegExp('^' + alias.key);
+  /**
+   * A callback function for transform-jest-deps.
+   * @param {string} require - Content of a require() statement. Automatically passed by transform-jest-deps.
+   * @returns {string} Value passed in, either modifed by the function, or not.
+   */
 
-    if (regex.test(require)) {
-      return require.replace(regex, alias.value);
+  function resolve(require) {
+    for (let i = 0; i < aliases.length; i++) {
+      const alias = aliases[i];
+      const regex = new RegExp('^' + alias.key);
+
+      if (regex.test(require)) {
+        return require.replace(regex, alias.value);
+      }
     }
+
+    return require;
   }
 
-  return require;
+  return {
+    process(src, path) {
+      src = babelJest.process(src, path);
+      src = transform(src, resolve);
+
+      return src;
+    },
+  };
 }
 
-module.exports = {
-  process(src, path) {
-    src = babelJest.process(src, path);
-    src = transform(src, resolve);
-
-    return src;
-  },
-};
+module.exports = preprocessorFactory;
