c647e0f9c4a77ec8300d992d32a0cc0ab98424ba
Dominik Rowicki <dominik.rowicki@netguru.co>
2016-12-26 23:40:37 +0100

    Tidied up the code, and updated the documentation

--- index.js
+++ index.js
@@ -7,12 +7,15 @@ const transform = require('transform-jest-deps');
  * Creates a new preprocessor, based on options given by the user.
  * @param {Object} config - Configuration object containing resolve rules. Can be an imported `webpack.config.js`.
  * @param {Object} config.resolve - A required property of config.
- * @param {string} config.resolve.root - The objective path to the root directory of your project. You can use `process.cwd()` to get the current working directory.
+ * @param {string} [config.resolve.root] - The objective path to the root directory of your project. You can use `process.cwd()` to get the current working directory. This is used for the original Webpack configurations.
+ * @param {string[]} [config.resolve.modules] - An array of paths providing context for resolving paths. By default contains only the `node_modules` directory, you should add some appropriate directory in your project in which aliases are contained. This directory can be the same that you would use for `config.resolve.root`. This property appears in Webpack 2 configurations.
  * @param {Object} config.resolve.alias - An object containing alias rules as string properties, where the key is an alias to be substituted, and the value is a path to the aliased file or directory. Each path has to be relative to the `config.resolve.root` property.
  * @returns {Object} - An object containing a `process` property, used as a preprocessor.
  */
 function preprocessorFactory(config) {
-  const roots = config.resolve.root ? [config.resolve.root] : config.resolve.modules;
+  const roots = config.resolve.root
+    ? [config.resolve.root]
+    : config.resolve.modules;
   const aliases = flatten(
     roots.map(root => Object.keys(config.resolve.alias)
       .map(key => {
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,6 +1,7 @@
 jest.autoMockOff();
 
-const config = {
+const preprocessorHoc = require('../index.js');
+const webpackConfig = {
   resolve: {
     root: './__fixtures__/',
     alias: {
@@ -10,136 +11,101 @@ const config = {
     },
   },
 };
-
-const preprocessorHoc = require('../index.js');
-const preprocessor = preprocessorHoc(config);
+const webpack2Config = {
+  resolve: {
+    modules: [
+      'node_modules',
+      '__fixtures__/app',
+      '__fixtures__/',
+    ],
+    alias: {
+      _js: 'app/js',
+      _utils: 'app/js/utils',
+      _helper: 'js/utils/helper.js',
+    },
+  },
+};
 
 describe('Module index.js', () => {
   it('should return an object with a process() property', () => {
-    const callResult = preprocessor;
+    const callResult = preprocessorHoc(webpackConfig);
 
     expect(callResult).toBeDefined();
-
     expect(callResult.process).toBeDefined();
   });
 });
 
-describe('The process() method', () => {
+describe('The process() method', testProcessMethod(webpackConfig));
+describe('webpack 2', testProcessMethod(webpack2Config));
+
+function testProcessMethod(config) {
   const filename = '__fixtures__/someDir/index.js';
+  const preprocessor = preprocessorHoc(config);
 
-  it('should call transform-jest-deps with a source to transpile', () => {
-    jest.mock('transform-jest-deps');
+  return () => {
+    it('should call transform-jest-deps with a source to transpile', () => {
+      jest.mock('transform-jest-deps');
 
-    const src = '';
-    const transform = require('transform-jest-deps');
-    const altPreprocessor = require('../index.js')(config);
+      const src = '';
+      const transform = require('transform-jest-deps');
+      const altPreprocessor = require('../index.js')(config);
 
-    altPreprocessor.process(src, filename);
+      altPreprocessor.process(src, filename);
+      expect(transform.mock.calls.length).toBeGreaterThan(0);
 
-    expect(transform.mock.calls.length > 0).toEqual(true);
-  });
+      jest.unmock('transform-jest-deps');
+    });
 
-  it('should return source unchanged if aliases have not been used', () => {
-    const src = 'require("file1.js")';
-    const src2 = '';
+    it('should return source unchanged if aliases have not been used', () => {
+      const src = 'require("file1.js")';
+      const src2 = '';
+      const callResult = preprocessor.process(src, filename);
+      const callResult2 = preprocessor.process(src2, filename);
 
-    const callResult = preprocessor.process(src, filename);
-    const callResult2 = preprocessor.process(src2, filename);
+      expect(callResult).toEqual(src);
+      expect(callResult2).toEqual(src2);
+    });
 
-    expect(callResult).toEqual(src);
-    expect(callResult2).toEqual(src2);
-  });
+    it('should convert aliases into real paths', () => {
+      let src;
 
-  it('should tolerate files without extensions', () => {
-    const src = 'require("_utils/helper")';
+      src = 'require("_js/index.js")';
+      expect(preprocessor.process(src, filename))
+        .toEqual('require("__fixtures__/app/js/index.js")');
 
-    expect(preprocessor.process(src, filename))
-    .toEqual('require("__fixtures__/app/js/utils/helper")');
-  });
+      src = 'require("_js/utils/helper.js")';
+      expect(preprocessor.process(src, filename))
+        .toEqual('require("__fixtures__/app/js/utils/helper.js")');
 
-  it('should convert aliases into absolute paths', () => {
-    let src = 'require("_js/index.js")';
+      src = 'require("_js/index.js"); require("_utils/helper.js")';
+      expect(preprocessor.process(src, filename))
+        .toEqual(
+          'require("__fixtures__/app/js/index.js");' +
+          ' require("__fixtures__/app/js/utils/helper.js")'
+        );
+    });
 
-    expect(preprocessor.process(src, filename))
-    .toEqual('require("__fixtures__/app/js/index.js")');
+    it('should handle files without extensions', () => {
+      const src = 'require("_utils/helper")';
 
-    src = 'require("_js/utils/helper.js")';
+      expect(preprocessor.process(src, filename))
+        .toEqual('require("__fixtures__/app/js/utils/helper")');
+    });
 
-    expect(preprocessor.process(src, filename))
-    .toEqual('require("__fixtures__/app/js/utils/helper.js")');
+    it('should handle directories as well as regular files', () => {
+      let src;
 
-    src = 'require("_js/index.js"); require("_utils/helper.js")';
+      src = 'require("_js")';
+      expect(preprocessor.process(src, filename))
+        .toEqual('require("__fixtures__/app/js")');
 
-    expect(preprocessor.process(src, filename))
-    .toEqual('require("__fixtures__/app/js/index.js");' +
-      ' require("__fixtures__/app/js/utils/helper.js")');
+      src = 'require("_js/")';
+      expect(preprocessor.process(src, filename))
+        .toEqual('require("__fixtures__/app/js/")');
 
-    src = 'require("_js")';
-
-    expect(preprocessor.process(src, filename))
-    .toEqual('require("__fixtures__/app/js")');
-
-    src = 'require("_js/")';
-
-    expect(preprocessor.process(src, filename))
-    .toEqual('require("__fixtures__/app/js/")');
-
-    src = 'require("_helper")';
-
-    expect(preprocessor.process(src, filename))
-    .toEqual('require("__fixtures__/app/js/utils/helper.js")');
-  });
-});
-
-describe('webpack 2', () => {
-  const filename = '__fixtures__/someDir/index.js';
-
-  it('should work with webpack2 config', () => {
-    const webpack2Config = {
-      resolve: {
-        modules: [
-          'node_modules',
-          '__fixtures__/app',
-          '__fixtures__/',
-        ],
-        alias: {
-          _js: 'app/js',
-          _utils: 'app/js/utils',
-          _helper: 'js/utils/helper.js',
-        },
-      },
-    };
-    const preprocessorWP2 = preprocessorHoc(webpack2Config);
-
-    let src = 'require("_js/index.js")';
-
-    expect(preprocessorWP2.process(src, filename))
-      .toEqual('require("__fixtures__/app/js/index.js")');
-
-    src = 'require("_js/utils/helper.js")';
-
-    expect(preprocessorWP2.process(src, filename))
-      .toEqual('require("__fixtures__/app/js/utils/helper.js")');
-
-    src = 'require("_js/index.js"); require("_utils/helper.js")';
-
-    expect(preprocessorWP2.process(src, filename))
-      .toEqual('require("__fixtures__/app/js/index.js");' +
-        ' require("__fixtures__/app/js/utils/helper.js")');
-
-    src = 'require("_js")';
-
-    expect(preprocessorWP2.process(src, filename))
-      .toEqual('require("__fixtures__/app/js")');
-
-    src = 'require("_js/")';
-
-    expect(preprocessorWP2.process(src, filename))
-      .toEqual('require("__fixtures__/app/js/")');
-
-    src = 'require("_helper")';
-
-    expect(preprocessorWP2.process(src, filename))
-      .toEqual('require("__fixtures__/app/js/utils/helper.js")');
-  });
-});
+      src = 'require("_helper")';
+      expect(preprocessor.process(src, filename))
+        .toEqual('require("__fixtures__/app/js/utils/helper.js")');
+    });
+  };
+}
